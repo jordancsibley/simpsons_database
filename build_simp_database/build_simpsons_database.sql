fa8c81aca580add551d3131e802933e0238f1c7f
Jordan Sibley <jordan.c.sibley@gmail.com>
2025-05-13 12:58:13 -0700

    Insert datafiles into database

--- build_simp_database/build_simpsons_database.sql
+++ build_simp_database/build_simpsons_database.sql
@@ -8,6 +8,11 @@ CREATE TABLE characters(
     gender VARCHAR, 
     PRIMARY KEY (id)
 );
+-- insert data into characters table
+INSERT INTO characters
+SELECT id, name, normalized_name, gender
+FROM read_csv_auto('/Users/jordansibley/Desktop/MEDS/EDS_213/section_database_proj/simpsons_database/data/characters_clean.csv', HEADER=TRUE, SAMPLE_SIZE=-1, NULLSTR = 'NA');
+
 
 -- define locations table 
 CREATE TABLE locations(
@@ -17,12 +22,17 @@ CREATE TABLE locations(
     PRIMARY KEY (id)
 );
 
+-- insert data into locations table 
+INSERT INTO locations
+SELECT id, name, normalized_name
+FROM read_csv_auto('/Users/jordansibley/Desktop/MEDS/EDS_213/section_database_proj/simpsons_database/data/locations_clean.csv', HEADER=TRUE, SAMPLE_SIZE=-1, NULLSTR = 'NA');
+
 -- define episodes table 
 CREATE TABLE episodes (
     id INTEGER NOT NULL,
     image_url VARCHAR,
-    imbd_rating INTEGER,
-    imbd_votes INTEGER,
+    imdb_rating INTEGER,
+    imdb_votes INTEGER,
     number_in_season INTEGER,
     number_in_series INTEGER,
     original_air_date DATE,
@@ -31,9 +41,15 @@ CREATE TABLE episodes (
     title VARCHAR,
     us_viewers_in_millions INTEGER,
     video_url VARCHAR,
-    view INTEGER,
+    views INTEGER,
     PRIMARY KEY (id)
 );
+
+-- insert data into episodes table 
+INSERT INTO episodes
+SELECT id, image_url, imdb_rating, imdb_votes, number_in_season, number_in_series, original_air_date, production_code, season, title, us_viewers_in_millions, video_url, views
+FROM read_csv_auto('/Users/jordansibley/Desktop/MEDS/EDS_213/section_database_proj/simpsons_database/data/episodes_clean.csv', HEADER=TRUE, SAMPLE_SIZE=-1, NULLSTR = 'NA');
+
 
 -- define script_lines table with foreign keys
 CREATE TABLE script_lines (
@@ -43,8 +59,8 @@ CREATE TABLE script_lines (
     raw_text VARCHAR,
     timestamp_in_ms INTEGER,
     speaking_line BOOLEAN,
-    character_id INTEGER NOT NULL,
-    location_id INTEGER NOT NULL,
+    character_id INTEGER,
+    location_id INTEGER,
     raw_character_text VARCHAR,
     raw_location_text VARCHAR,
     spoken_words VARCHAR,
@@ -58,8 +74,8 @@ CREATE TABLE script_lines (
     FOREIGN KEY (episode_id) REFERENCES episodes(id)
 );
 
--- add data files into database
-COPY characters FROM 'data/characters_clean.csv' (header TRUE, DELIM ',');
-COPY locations FROM 'data/simpsons_locations.csv' (header TRUE, DELIM ',');
-COPY episodes FROM 'data/simpsons_locations.csv' (header TRUE, DELIM ',');
-COPY script_lines FROM 'data/simpsons_script_lines.csv' (header TRUE, DELIM ',');
+-- insert data into script_lines table 
+INSERT INTO script_lines
+SELECT id, episode_id, number, raw_text, timestamp_in_ms, speaking_line, character_id, location_id, raw_character_text, raw_location_text, spoken_words, normalized_text, word_count
+FROM read_csv_auto('/Users/jordansibley/Desktop/MEDS/EDS_213/section_database_proj/simpsons_database/data/script_lines_clean.csv', HEADER=TRUE, SAMPLE_SIZE=-1, NULLSTR = 'NA');
+
